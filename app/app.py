7c6c46aa62d2920dd6e1daa57b2b985d74be6e8d
thepm-cpu <thepm002@gmail.com>
2025-07-23 00:09:41 +0100

    ✨ Add /about route with CI/CD info

--- app/app.py
+++ app/app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"<h1>Hello from CI/CD 🚀</h1><p>Deployed at: {datetime.utcnow()} UTC</p>"
+    return f"""
+        <h1>Hello from CI/CD 🚀</h1>
+        <p>Deployed at: {datetime.utcnow()} UTC</p>
+        <p><a href='/about'>About</a></p>
+    """
+
+@app.route("/about")
+def about():
+    return "<h2>This is a Flask app deployed via GitHub Actions to a DigitalOcean Droplet 🐳</h2>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
